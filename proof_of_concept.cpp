9dec95f31b4bb5c1e4ca21ce0d7fe5bf16f6b3b3
Max Ellsworth <max.ellsworth@gmail.com>
2021-04-25 16:52:45 -0400

    Got bit reversal

--- proof_of_concept.cpp
+++ proof_of_concept.cpp
@@ -3,29 +3,52 @@
 #include <iostream>
 #include <complex>
 #include <immintrin.h>
+#include <bitset>
 
 #define PI 3.14159265358979323846
 #define PERIODS 1
 #define FREQ	100
 using namespace std;
 
+
 void proof_of_concept(){	
 	int freq = 10;
 	int periods = 1;
 	int len = (int) 2 * PI * periods * freq;
 	int index = 0;
-
+	float input[len];
 	__m256 blocks[len];
+
+	for (int i = 0; i < len; i++) {input[i] = i;}
 
 	for (int j = 0; j < len; j += 8) {
 	//	cout << "j : " << j << endl;
 
+
 	float * a_float = new (std::align_val_t(32)) float[8];
 	float * b_float = new (std::align_val_t(32)) float[8];
 
-	for (int i = 0; i < 8; i++) a_float[i] = 10;// * sin((float)i/freq);
-	for (int i = 0; i < 8; i++) b_float[i] = sin(((float)i + index)/freq);
+	for (int i = 0; i < 8; i++) a_float[i] = input[index + i];//= 10;// * sin((float)i/freq);
+	for (int i = 0; i < 8; i++) {
+		switch(i) {
+			float temp;
+			case 1: 
+				temp = a_float[1];
+				b_float[1] = a_float[4];
+				b_float[4] = temp;
+				break;
+			case 3: 
+				temp = a_float[3];
+				b_float[3] = a_float[6];
+				b_float[6] = temp;
+				break;
+			default: b_float[i] = a_float[i];
+		} 
 
+		//cout << i << " : " << b_float[i] << endl;
+		}
+//	cout << i << " : " << (bs) << " : " << (bs >> 1) << endl;}// b_float[i] = 2 * input[index + i]; //sin(((float)i + index)/freq);
+//	float b_float[] = {a_float[0],a_float[4], a_float[2], a_float[6], a_float[1], a_float[5], a_float[2], a_float[7]}; 
 
 //	for (int i = 0; i < len; i++) cout << a_float[i] << endl;
 
@@ -40,11 +63,16 @@ void proof_of_concept(){
 
 	_mm256_store_ps(c_float,c);
 
-	//for (int i = 0; i < 8; i++) cout << c_float[i] << endl;
+//	for (int i = 0; i < 8; i++) cout << c_float[i] << endl;
 	index += 8;
 }
 	float * d_float = new (std::align_val_t(32)) float[8];	
 
 	_mm256_store_ps(d_float, blocks[0]);
-	for (int i = 0; i < 8; i++) cout << d_float[i] << endl;
+//	for (int i = 0; i < 8; i++) cout << d_float[i] << endl;
+}
+
+int main(){
+	proof_of_concept();
+	return 0;
 }
